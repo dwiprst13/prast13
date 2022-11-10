87e6ff638dfbea47ea38ead2a19759062eb2a319
prast13 <dwiprst13@gmail.com>
2022-11-10 18:27:31 +0700

    coba coba

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const txtElement = ['useles', 'unworthly', 'ugly', 'boring person'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+(function ngetik(){
+    if(count == txtElement.length){
+        count = 0;
+    }
+    currentTxt = txtElement[count];
+    words =currentTxt.slice(0, ++txtIndex);
+    document.querySelector('.efek').textContent = words;
+    if(words.length == currentTxt.length){
+        count++;
+        txtIndex = 0;
+    }
+    setTimeout(ngetik, 300)
+})();
